40c6256651b06ee80af62e5af3f21ec84825cdaf
Autumn Meadow <blazeykirin@gmail.com>
2022-12-18 21:12:21 -0800

    add spoolsize

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,11 +1,16 @@
-type MainResult<T = ()> = Result<T, Box<dyn std::error::Error>>;
+pub const DEFAULT_SPOOL_INMEMORY_SIZE: usize = 5242880; // 5 * 1024 * 1024, 5MiB
 
 pub struct WiwiBlob {
-	dir: String
+	dir: String,
+	spoolsize: usize
 }
 
 impl WiwiBlob {
 	pub fn new(dir: String) -> Self {
-		Self { dir }
+		Self::with_spoolsize(dir, DEFAULT_SPOOL_INMEMORY_SIZE)
+	}
+
+	pub fn with_spoolsize(dir: String, spoolsize: usize) -> Self {
+		Self { dir, spoolsize }
 	}
 }
