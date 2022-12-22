7752b5e3486932b33d95b714b2fed08d17667506
Autumn Meadow <blazeykirin@gmail.com>
2022-12-21 23:42:14 -0500

    more errors

--- lib/src/error.rs
+++ lib/src/error.rs
@@ -7,5 +7,7 @@ pub enum Error {
 	#[error("The provided owner is too long, expected less than {}, got {}", u8::MAX, .0)]
 	OwnerTooLong(usize),
 	#[error("The provided hash does not match with the actual hash, expected {expected}, got {got}")]
-	MismatchedHash { expected: String, got: String }
+	MismatchedHash { expected: String, got: String },
+	#[error("unknown metadata field: {}", .0)]
+	UnknownMetaField(String)
 }
