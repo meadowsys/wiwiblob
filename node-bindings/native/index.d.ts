0a31d30085a14c56d1eeb0108bda83745cbbc527
Autumn Meadow <blazeykirin@gmail.com>
2022-12-24 12:10:37 -0500

    reorganise typings

--- node-bindings/native/index.d.ts
+++ node-bindings/native/index.d.ts
@@ -1,24 +1,18 @@
 declare class Nominal<T> { #__private: T }
 
 declare namespace native {
-	export namespace wiwiblob {
-		export type WiwiBlob = Nominal<WiwiBlob>;
+	export type WiwiBlob = Nominal<WiwiBlob>;
+	export type Reader = Nominal<Reader>;
+	export type ReaderBuilder = Nominal<ReaderBuilder>;
+	export type Writer = Nominal<Writer>;
+	export type WriterBuilder = Nominal<WriterBuilder>;
 
+	export namespace wiwiblob {
 		export function new_wiwiblob(dir: string): WiwiBlob;
 		export function new_wiwiblob_with_spoolsize(dir: string, spoolsize: number): WiwiBlob;
-		export function reader_builder(wiwiblob: WiwiBlob, hash: string): read.ReaderBuilder
-		export function writer_builder(wiwiblob: WiwiBlob): write.WriterBuilder
-		export function writer_builder_with_spoolsize(wiwiblob: WiwiBlob, spoolsize: number): write.WriterBuilder
-	}
-
-	export namespace read {
-		export type Reader = Nominal<Reader>;
-		export type ReaderBuilder = Nominal<ReaderBuilder>;
-	}
-
-	export namespace write {
-		export type Writer = Nominal<Writer>;
-		export type WriterBuilder = Nominal<WriterBuilder>;
+		export function reader_builder(wiwiblob: WiwiBlob, hash: string): ReaderBuilder
+		export function writer_builder(wiwiblob: WiwiBlob): WriterBuilder
+		export function writer_builder_with_spoolsize(wiwiblob: WiwiBlob, spoolsize: number): WriterBuilder
 	}
 }
 
