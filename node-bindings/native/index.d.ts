f4a0ecc906d7ecde64e576b37ac817c8d4f70eb9
Autumn Meadow <blazeykirin@gmail.com>
2023-01-11 11:47:12 -0800

    todo markers E

--- node-bindings/native/index.d.ts
+++ node-bindings/native/index.d.ts
@@ -8,33 +8,33 @@ declare namespace native {
 	export type WriterBuilder = Nominal<WriterBuilder>;
 
 	export namespace wiwiblob {
-		export function new_wiwiblob(dir: string): WiwiBlob;
-		export function new_wiwiblob_with_spoolsize(dir: string, spoolsize: number): WiwiBlob;
-		export function reader_builder(wiwiblob: WiwiBlob, hash: string): ReaderBuilder;
-		export function writer_builder(wiwiblob: WiwiBlob): WriterBuilder;
-		export function writer_builder_with_spoolsize(wiwiblob: WiwiBlob, spoolsize: number): WriterBuilder;
+		/* TODO promisify? */ export function new_wiwiblob(dir: string): WiwiBlob;
+		/* TODO promisify? */ export function new_wiwiblob_with_spoolsize(dir: string, spoolsize: number): WiwiBlob;
+		/* TODO promisify? */ export function reader_builder(wiwiblob: WiwiBlob, hash: string): ReaderBuilder;
+		/* TODO promisify? */ export function writer_builder(wiwiblob: WiwiBlob): WriterBuilder;
+		/* TODO promisify? */ export function writer_builder_with_spoolsize(wiwiblob: WiwiBlob, spoolsize: number): WriterBuilder;
 	}
 
 	export namespace reader_builder {
-		export function verify(reader_builder: ReaderBuilder, verify: boolean): void;
-		export function build(reader_builder: ReaderBuilder): Reader;
+		/* TODO promisify? */ export function verify(reader_builder: ReaderBuilder, verify: boolean): void;
+		/* TODO promisify? */ export function build(reader_builder: ReaderBuilder): Reader;
 	}
 
 	export namespace reader {
-		export function get_filename(reader: Reader): string | undefined;
-		export function get_owner(reader: Reader): string | undefined;
-		export function read_to_new_buffer(reader: Reader, bufsize: number): [Buffer, number];
+		/* TODO promisify? */ export function get_filename(reader: Reader): string | undefined;
+		/* TODO promisify? */ export function get_owner(reader: Reader): string | undefined;
+		/* TODO promisify? */ export function read_to_new_buffer(reader: Reader, bufsize: number): [Buffer, number];
 	}
 
 	export namespace writer_builder {
-		export function set_filename(writer_builder: WriterBuilder, filename: string): void;
-		export function set_owner(writer_builder: WriterBuilder, owner: string): void;
-		export function build(writer_builder: WriterBuilder): Writer;
+		/* TODO promisify? */ export function set_filename(writer_builder: WriterBuilder, filename: string): void;
+		/* TODO promisify? */ export function set_owner(writer_builder: WriterBuilder, owner: string): void;
+		/* TODO promisify? */ export function build(writer_builder: WriterBuilder): Writer;
 	}
 
 	export namespace writer {
-		export function write_all(writer: Writer, buf: Buffer): void;
-		export function finish(writer: Writer): string;
+		/* TODO promisify? */ export function write_all(writer: Writer, buf: Buffer): void;
+		/* TODO promisify? */ export function finish(writer: Writer): string;
 	}
 }
 
