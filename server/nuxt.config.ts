27c757a0903c03c98f1ca014cc4814231dc10fdf
Autumn Meadow <blazeykirin@gmail.com>
2023-01-17 00:04:44 -0800

    typescript options

--- server/nuxt.config.ts
+++ server/nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-	telemetry: false
+	telemetry: false,
+	typescript: {
+		shim: false,
+		strict: true,
+		typeCheck: "build"
+	}
 });
