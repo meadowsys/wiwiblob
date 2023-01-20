436b70ffe74166326abe9be499c22f289b549374
Autumn Meadow <blazeykirin@gmail.com>
2023-01-20 01:15:30 -0800

    commit the UI oops i forgot

--- server/pages/index.vue
+++ server/pages/index.vue
@@ -6,10 +6,26 @@
 				wiwiblob
 			</div>
 			<div class="h-24" />
-			<form method="post" action="/api/upload/public-file">
-				<input ref="input" type="file" name="files" multiple>
-				<br>
-				<input type="submit" value="Upload" formenctype="multipart/form-data">
+			<form method="post" action="/api/upload/public-file" class="border-2 border-purple-500 rounded-lg">
+				<input
+					ref="input"
+					type="file"
+					name="files"
+					class="m-7 p-1"
+					multiple
+					required
+				>
+				<input
+					type="submit"
+					value="Upload"
+					formenctype="multipart/form-data"
+					class="
+						mr-6 px-4 py-2
+						border border-purple-300 rounded-md
+						hover:bg-purple-200 hover:border-purple-500 active:bg-purple-300 transition-colors
+						cursor-pointer
+					"
+				>
 			</form>
 		</div>
 		<div class="flex-grow" />
