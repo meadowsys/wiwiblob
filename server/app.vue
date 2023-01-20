cbfd54d965837e1f2b2f0d243a43241fd4d593a7
Autumn Meadow <blazeykirin@gmail.com>
2023-01-19 20:56:35 -0800

    basic form to submit formdata to the endpoint IT WORKS AAAAAAAAAAAAAAAAAAAA (i think)

--- server/app.vue
+++ server/app.vue
@@ -1,5 +1,5 @@
 <template>
-	<div>
-		<NuxtWelcome />
+	<div class="h-screen">
+		<nuxt-page />
 	</div>
 </template>
---
+++ server/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="h-full flex">
+		<div class="flex-grow" />
+		<div>
+			<div class="pt-10 text-3xl text-center">
+				wiwiblob
+			</div>
+			<div class="h-24" />
+			<form method="post" action="/api/upload/public-file">
+				<input ref="input" type="file" name="files" multiple>
+				<br>
+				<input type="submit" value="Upload" formenctype="multipart/form-data">
+			</form>
+		</div>
+		<div class="flex-grow" />
+	</div>
+</template>
