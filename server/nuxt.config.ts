d26605b9075cbf425246fb9a9a0eddf6cbe1963f
Autumn Meadow <blazeykirin@gmail.com>
2023-01-19 20:11:25 -0800

    install tailwind

--- server/nuxt.config.ts
+++ server/nuxt.config.ts
@@ -30,6 +30,9 @@ if (process.env.NODE_ENV === "production") {
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	telemetry: false,
+	modules: [
+		"@nuxtjs/tailwindcss"
+	],
 	typescript: {
 		shim: false,
 		strict: true,
