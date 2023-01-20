6996767f1352f6a0dabd0950aa494bd32f61e648
Autumn Meadow <blazeykirin@gmail.com>
2023-01-20 14:59:17 -0800

    fix file upload of large files?
    switch from builtin h3 to formidable, currently using tempfile implementation

--- server/server/api/upload/public-file.post.ts
+++ server/server/api/upload/public-file.post.ts
@@ -1,43 +1,58 @@
 import { use_wiwiblob } from "~/composables/use_wiwiblob";
-import { readMultipartFormData } from "h3";
+import { formidable, File } from "formidable";
+import fs from "fs";
 
 export const filetype_metakey = "filetype";
 
 export default defineEventHandler(async event => {
-	let wiwiblob = use_wiwiblob();
-
-	let formdata = await readMultipartFormData(event);
-	if (!formdata) {
-		event.node.res.statusCode = 400;
-		event.node.res.end();
-		return;
-	}
-
-	if (formdata.length > 5) {
-		event.node.res.statusCode = 400;
-		return { err: "upload at most 5 files at a time" };
-	}
+	let formdata = formidable({
+		allowEmptyFiles: false,
+		maxFileSize: 100 * 1024 * 1024 * 1024,
+		multiples: true,
+		keepExtensions: true
+	});
 
 	let promises: Array<Promise<string>> = [];
+	await new Promise<void>(res => {
+		formdata.parse(event.node.req, (err, fields, _files) => {
+			if (err) {
+				console.log(err);
+				event.node.res.statusCode = 500;
+				event.node.res.end();
+				return;
+			}
 
-	for (let file of formdata) {
-		let writer_builder = wiwiblob.writer_builder();
+			let files = Object.entries(_files);
+			if (files.length > 5) return { err: "upload at most 5 files at a time" };
 
-		console.log(file.type);
-		if (file.filename) writer_builder.set_filename(file.filename);
-		if (file.type) writer_builder.set_other_meta(file.type, file.type);
-		let writer = writer_builder.build();
-
-		promises.push(new Promise((res, rej) => {
-			writer.write(file.data, err => {
-				if (err) return rej(err);
-				writer.end(() => {
-					res(writer.get_hash());
-				});
-			});
-		}));
-	}
+			for (let [filekey, file] of files) {
+				if (Array.isArray(file)) {
+					promises.push(...file.map(process_file));
+				} else {
+					promises.push(process_file(file));
+				}
+			}
+			res();
+		});
+	});
 
 	let hashes = await Promise.all(promises);
 	return { hashes };
 });
+
+async function process_file(file: File): Promise<string> {
+	let wiwiblob = use_wiwiblob();
+
+	let writer_builder = wiwiblob.writer_builder();
+	if (file.originalFilename) writer_builder.set_filename(file.originalFilename);
+	if (file.mimetype) writer_builder.set_other_meta(filetype_metakey, file.mimetype);
+	let writer = writer_builder.build();
+
+	let read_stream = fs.createReadStream(file.filepath);
+	read_stream.pipe(writer);
+	return await new Promise(res => {
+		writer.on("close", () => {
+			res(writer.get_hash());
+		});
+	});
+}
