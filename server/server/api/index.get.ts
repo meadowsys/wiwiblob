549acd568a55f981dfd7eaea8987938bbc63c8b0
Autumn Meadow <blazeykirin@gmail.com>
2023-01-24 00:42:38 -0800

    calling top level /api now loads the db and the blobs

--- server/server/api/index.get.ts
+++ server/server/api/index.get.ts
@@ -1,3 +1,9 @@
+import { use_surrealdb } from "~/composables/use_surrealdb";
+import { use_wiwiblob } from "~/composables/use_wiwiblob";
+
 export default defineEventHandler(() => {
+	use_surrealdb();
+	use_wiwiblob();
+
 	return { staat: "goed (misschien)" };
 });
