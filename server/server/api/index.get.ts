e61c9e6eafdbbd8a17bf189ebf1e24b9cfb08ed0
Autumn Meadow <blazeykirin@gmail.com>
2023-01-24 01:09:43 -0800

    actually wait for finish initialising

--- server/server/api/index.get.ts
+++ server/server/api/index.get.ts
@@ -1,9 +1,9 @@
 import { use_surrealdb } from "~/composables/use_surrealdb";
 import { use_wiwiblob } from "~/composables/use_wiwiblob";
 
-export default defineEventHandler(() => {
-	use_surrealdb();
-	use_wiwiblob();
+export default defineEventHandler(async () => {
+	await use_surrealdb();
+	await use_wiwiblob();
 
 	return { staat: "goed (misschien)" };
 });
